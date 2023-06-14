0b35a904fea78848b23039f61f4ce071e8def4a9
ertebatat sahar <Yasinallahyari86@gmail.com>
2023-06-14 16:17:57 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TitlesSection from "./sections/TitlesSection";
+import AboutSection from "./sections/AboutSection";
+import SkillSection from "./sections/SkillSection";
+import { Link, Element } from "react-scroll";
+
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="navigation">
+          <div className="Right">
+          </div>
+            <div className="Left">
+              <Link
+                activeClass="active"
+                to="about"
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={500}
+                delay={0}
+              >
+              <h4>About</h4>
+              </Link>
+              <Link
+                activeClass="active"
+                to="skills"
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={500}
+                delay={0}
+              >
+                <h4>Skills</h4>
+              </Link>
+            </div>
+        </div>
+        <TitlesSection />
+        <Element name="about" className="element" />
+        <AboutSection />
+        <Element name="skills" className="element" />
+        <SkillSection />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/sections/AboutSection.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Fullpage from "../components/Fullpage";
+import data from "../data.json";
+import './AboutSection.css';
+import { Link, Element } from "react-scroll";
+
+
+class AboutSection extends Component {
+  render() {
+    return (
+      <Fullpage className="second">
+        <h3>{data.sections[0].title}</h3>
+        <div>
+          {data.sections[0].items.map(p => {
+            return <p>{p.content}</p>;
+          })}
+        </div>
+      </Fullpage>
+    );
+  }
+}
+
+export default AboutSection;
